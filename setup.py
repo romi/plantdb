f964ca54c3d4f41c7d0e9b3e6c5a499e01215cfc
Timothée Wintz <timothee@timwin.fr>
2019-11-29 12:23:03 +0100

    (temporarily remove deps)

--- setup.py
+++ setup.py
@@ -17,8 +17,6 @@ opts = dict(name=NAME,
                 "luigi",
                 "numpy",
                 "imageio",
-                "dirsync",
-                "watchdog"
             ],
             )
 
