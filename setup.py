2a0ac0cb9aeb0805706ef47201f6f85dcef0f099
Timothée Wintz <timothee@timwin.fr>
2020-02-07 10:30:59 +0100

    begin refactor

--- setup.py
+++ setup.py
@@ -4,9 +4,12 @@ import subprocess
 
 opts = dict(name="romidata",
             packages=find_packages(),
-            scripts=['bin/fsdb-sync'],
+            scripts=['bin/fsdb-sync', 'bin/run-task', 'bin/scanner-rest-api'],
             setup_requires=['setuptools_scm'],
-            use_scm_version=True
+            use_scm_version=True,
+            install_requires=[
+                'luigi'
+            ]
             )
 
 if __name__ == '__main__':
