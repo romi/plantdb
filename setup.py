4512c270adfc9db2c96dcf662f10db13d443f5dc
Jonathan LEGRAND <jonathan.legrand@ens-lyon.fr>
2021-12-14 13:16:21 +0100

    Update requirements to use imageio2.9

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
-from setuptools import setup, find_packages
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import find_packages
+from setuptools import setup
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 opts = dict(
     name="plantdb",
-    packages=find_packages(),
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    include_package_data=False,
     scripts=[
         'bin/romi_fsdb_sync',
         'bin/romi_import_folder',
@@ -16,19 +21,8 @@ opts = dict(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://docs.romi-project.eu/Scanner/",
-    use_scm_version=True,
-    setup_requires=['setuptools_scm'],
-    install_requires=[
-        'appdirs',
-        'dirsync',
-        'flask',
-        'flask-cors',
-        'flask-restful',
-        'imageio',
-        'numpy',
-        'pillow',
-        'toml'
-    ],
+    package_data ={"tests": ["test_fsdb.py", "test_io.py"]},
+    zip_safe=False,
     python_requires='>=3.6',
     classifiers=[
         "Programming Language :: Python :: 3",
@@ -36,8 +30,6 @@ opts = dict(
         "Intended Audience :: Science/Research",
         "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)"
     ],
-    # If True, include any files specified by your MANIFEST.in:
-    include_package_data=False
 )
 
 if __name__ == '__main__':
