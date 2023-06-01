ce8c79d4d9490e6901f6dc2e9958adebc9f717b1
Jonathan LEGRAND <jlegrand62@gmail.com>
2023-06-01 12:17:22 +0200

    Duplicate log system from `romitask`

--- src/plantdb/log.py
+++ src/plantdb/log.py
@@ -23,9 +23,90 @@
 # <https://www.gnu.org/licenses/>.
 # ------------------------------------------------------------------------------
 
-try:
-    from romitask.log import configure_logger
-except Exception:
-    import logging
-    def configure_logger(name):
-        return logging.getLogger('name')
+import logging
+from pathlib import Path
+
+from colorlog import ColoredFormatter
+
+LOGLEV = ["NOTSET", "DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"]
+
+LOGGING_CFG = """
+    [loggers]
+    keys=root
+
+    [logger_root]
+    handlers=console
+    qualname={0}
+    level={1}
+
+    [handlers]
+    keys=console
+
+    [handler_console]
+    class=logging.StreamHandler
+    formatter=color
+    level={1}
+    stream  : ext://sys.stdout
+
+    [formatters]
+    keys=color
+
+    [formatter_color]
+    class=colorlog.ColoredFormatter
+    format=%(log_color)s%(levelname)-8s%(reset)s %(bg_blue)s[%(name)s]%(reset)s %(message)s
+    datefmt=%m-%d %H:%M:%S
+    """
+
+
+def get_logging_config(name='root', log_level='INFO'):
+    """Return the logging configuration.
+
+    Parameters
+    ----------
+    name : str
+        The name of the logger.
+        Defaults to `'root'`.
+    log_level : {'CRITICAL', 'ERROR', 'WARNING', 'INFO', 'DEBUG', 'NOTSET'}
+        A valid logging level.
+        Defaults to `'INFO'`.
+    """
+    return LOGGING_CFG.format(name, log_level)
+
+
+def configure_logger(name, log_path="", log_level='INFO'):
+    """Return a configured logger.
+
+    Parameters
+    ----------
+    name : str
+        The name of the logger.
+    log_path : str
+        A file path to save the log.
+        Defaults to `''`.
+    log_level : {'CRITICAL', 'ERROR', 'WARNING', 'INFO', 'DEBUG', 'NOTSET'}
+        A valid logging level.
+        Defaults to `'INFO'`.
+    """
+    colored_formatter = ColoredFormatter(
+        "%(log_color)s%(levelname)-8s%(reset)s %(bg_blue)s[%(name)s]%(reset)s %(message)s",
+        datefmt=None,
+        reset=True,
+        style='%',
+    )
+    simple_formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(name)s - %(lineno)d: %(message)s")
+
+    # create console handler:
+    console = logging.StreamHandler()
+    console.setFormatter(colored_formatter)
+
+    logger = logging.getLogger(name)
+    logger.addHandler(console)
+    logger.setLevel(getattr(logging, log_level))
+
+    if log_path is not None and log_path != "":
+        # create file handler:
+        fh = logging.FileHandler(Path(log_path) / f'{name}.log', mode='w')
+        fh.setFormatter(simple_formatter)
+        logger.addHandler(fh)
+
+    return logger
