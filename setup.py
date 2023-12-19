8c58883b35fdbea8dcae2440c6bb62aa1c988672
Jonathan Legrand <jonathan.legrand@ens-lyon.fr>
2023-12-19 11:40:43 +0100

    Set usable Python versions to 3.8 & 3.9.
    No `open3d` conda package for:
      * open3d>= 0.15.1
      * Python>= 3.9
    
    See:
      * [anaconda](https://anaconda.org/open3d-admin/open3d/files)
      * [PyPi](https://pypi.org/project/open3d/)

--- setup.py
+++ setup.py
@@ -24,9 +24,11 @@ opts = dict(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://docs.romi-project.eu/plant_imager/",
-    package_data={"tests": ["test_fsdb.py", "test_io.py", "test_sync.py"]},
+    package_data={"tests":
+                      ["test_fsdb.py", "test_io.py", "test_romi_import_images.py", "test_sync.py", "test_webcache.py"]
+                  },
     zip_safe=False,
-    python_requires='>=3.8',
+    python_requires='>=3.8,<3.10',
     classifiers=[
         "Programming Language :: Python :: 3",
         "Development Status :: 4 - Beta",
