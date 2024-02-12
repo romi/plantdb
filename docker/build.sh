b8c1c131bbe17519fbcf715df0bf2a7bfabcad3d
Jonathan LEGRAND <jonathan.legrand@ens-lyon.fr>
2024-02-12 17:17:54 +0100

    Improve `Dockerfile`.
    Add `romidb` marker file to `$ROMI_DB` path.

--- docker/build.sh
+++ docker/build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # - Defines colors and message types:
 RED="\033[0;31m"
-GREEN="\033[0;32m"
 YELLOW="\033[0;33m"
+GREEN="\033[0;32m"
 NC="\033[0m" # No Color
 INFO="${GREEN}INFO${NC}    "
 WARNING="${YELLOW}WARNING${NC} "
