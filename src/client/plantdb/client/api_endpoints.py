dc5b7b8a19eb6074387cceb6d0c91e94a312cd4e
Jonathan Legrand <jonathan.legrand@ens-lyon.fr>
2026-03-04 14:47:23 +0100

    Reorganize endpoints and improve docstrings

--- src/client/plantdb/client/api_endpoints.py
+++ src/client/plantdb/client/api_endpoints.py
@@ -76,6 +76,71 @@ def url_prefix(endpoint_path):
     return wrapper
 
 
+# ------------------------------------------------------------------------
+# - Base Endpoints
+# ------------------------------------------------------------------------
+
+@url_prefix
+def health(**kwargs) -> str:
+    """Return the URL path to the health endpoint.
+
+    Other Parameters
+    ----------------
+    prefix : str
+        An optional prefix to prepend to the URL path.
+
+    Returns
+    -------
+    str
+        The URL path to the health endpoint.
+
+    Examples
+    --------
+    >>> from plantdb.client import api_endpoints
+    >>> api_endpoints.health(prefix='/api/v1')
+    '/api/v1/health'
+    """
+    return "/health"
+
+
+@url_prefix
+def refresh(scan_id: str = None, **kwargs) -> str:
+    """Return the URL path to the dataset archive endpoint.
+
+    Parameters
+    ----------
+    scan_id : str
+        The name of the scan dataset to archive.
+
+    Other Parameters
+    ----------------
+    prefix : str
+        An optional prefix to prepend to the URL path.
+
+    Returns
+    -------
+    str
+        The URL path to the refresh endpoint.
+
+    Examples
+    --------
+    >>> from plantdb.client import api_endpoints
+    >>> api_endpoints.refresh(prefix='/api/v1')
+    '/api/v1/refresh'
+    >>> api_endpoints.refresh('scan1')
+    '/refresh?scan_id=scan1'
+    """
+    params = ""
+    if scan_id:
+        scan_id = sanitize_name(scan_id)
+        params = f"?scan_id={scan_id}"
+    return f"/refresh{params}"
+
+
+# ------------------------------------------------------------------------
+# - Authentication Endpoints
+# ------------------------------------------------------------------------
+
 @url_prefix
 def register(**kwargs) -> str:
     """Return the URL path to the register endpoint.
@@ -214,61 +279,9 @@ def create_api_token():
     return "/create-api-token"
 
 
-@url_prefix
-def health(**kwargs) -> str:
-    """Return the URL path to the health endpoint.
-
-    Other Parameters
-    ----------------
-    prefix : str
-        An optional prefix to prepend to the URL path.
-
-    Returns
-    -------
-    str
-        The URL path to the health endpoint.
-
-    Examples
-    --------
-    >>> from plantdb.client import api_endpoints
-    >>> api_endpoints.health(prefix='/api/v1')
-    '/api/v1/health'
-    """
-    return "/health"
-
-
-@url_prefix
-def refresh(scan_id: str = None, **kwargs) -> str:
-    """Return the URL path to the dataset archive endpoint.
-
-    Parameters
-    ----------
-    scan_id : str
-        The name of the scan dataset to archive.
-
-    Other Parameters
-    ----------------
-    prefix : str
-        An optional prefix to prepend to the URL path.
-
-    Returns
-    -------
-    str
-        The URL path to the refresh endpoint.
-
-    Examples
-    --------
-    >>> from plantdb.client import api_endpoints
-    >>> api_endpoints.refresh(prefix='/api/v1')
-    '/api/v1/refresh'
-    >>> api_endpoints.refresh('scan1')
-    '/refresh?scan_id=scan1'
-    """
-    params = ""
-    if scan_id:
-        scan_id = sanitize_name(scan_id)
-        params = f"?scan_id={scan_id}"
-    return f"/refresh{params}"
+# ------------------------------------------------------------------------
+# - Scan, Fileset & File Endpoints
+# ------------------------------------------------------------------------
 
 
 @url_prefix
@@ -344,8 +357,198 @@ def scan(scan_id: str, **kwargs) -> str:
     >>> api_endpoints.scan('scan1', prefix='/api/v1')
     '/api/v1/scan/scan1'
     """
-    return f"/scan/{sanitize_name(scan_id)}"
+    scan_id = sanitize_name(scan_id)
+    return f"/scan/{scan_id}"
 
+
+@url_prefix
+def metadata_scan(scan_id: str, **kwargs) -> str:
+    """URL to access the metadata associated with the given scan name.
+
+    Parameters
+    ----------
+    scan_id : str
+        The name of the scan to access.
+
+    Other Parameters
+    ----------------
+    prefix : str
+        An optional prefix to prepend to the URL path.
+
+    Returns
+    -------
+    str
+        The URL path to scan metadata.
+    """
+    scan_id = sanitize_name(scan_id)
+    return f"/scan/{scan_id}/metadata"
+
+
+@url_prefix
+def list_scan_filesets(scan_id: str, **kwargs) -> str:
+    """URL to list the filesets associated with the given scan name.
+
+    Parameters
+    ----------
+    scan_id : str
+        The name of the scan to access.
+
+    Other Parameters
+    ----------------
+    prefix : str
+        An optional prefix to prepend to the URL path.
+
+    Returns
+    -------
+    str
+        The URL path to filesets.
+    """
+    scan_id = sanitize_name(scan_id)
+    return f"/scan/{scan_id}/filesets"
+
+
+@url_prefix
+def fileset(scan_id, fileset_id, **kwargs) -> str:
+    """URL path for a fileset belonging to a scan.
+
+    Parameters
+    ----------
+    scan_id : str
+        The name of the scan to access.
+    fileset_id : str
+        The name of the fileset to access.
+    file_id : str
+        The name of the file to access.
+
+    Other Parameters
+    ----------------
+    prefix : str
+        An optional prefix to prepend to the URL path.
+
+    Returns
+    -------
+    str
+        The URL path to file metadata.
+    """
+    scan_id = sanitize_name(scan_id)
+    fileset_id = sanitize_name(fileset_id)
+    return f"/fileset/{scan_id}/{fileset_id}"
+
+
+@url_prefix
+def metadata_fileset(scan_id: str, fileset_id: str, **kwargs) -> str:
+    """URL to access the fileset metadata associated with the given scan and fileset name.
+
+    Parameters
+    ----------
+    scan_id : str
+        The name of the scan to access.
+    fileset_id : str
+        The name of the fileset to access.
+
+    Other Parameters
+    ----------------
+    prefix : str
+        An optional prefix to prepend to the URL path.
+
+    Returns
+    -------
+    str
+        The URL path to fileset metadata.
+    """
+    scan_id = sanitize_name(scan_id)
+    fileset_id = sanitize_name(fileset_id)
+    return f"/scan/{scan_id}/{fileset_id}/metadata"
+
+
+@url_prefix
+def list_fileset_files(scan_id: str, fileset_id: str, **kwargs) -> str:
+    """URL to list the file associated with the given scan and filesets names.
+
+    Parameters
+    ----------
+    scan_id : str
+        The name of the scan to access.
+    fileset_id : str
+        The name of the fileset to access.
+
+    Other Parameters
+    ----------------
+    prefix : str
+        An optional prefix to prepend to the URL path.
+
+    Returns
+    -------
+    str
+        The URL path to the filesets list of files.
+    """
+    scan_id = sanitize_name(scan_id)
+    fileset_id = sanitize_name(fileset_id)
+    return f"/scan/{scan_id}/{fileset_id}/files"
+
+
+@url_prefix
+def file(scan_id: str, fileset_id: str, file_id: str, **kwargs) -> str:
+    """Return the URL path to the `scan/fileset/file` endpoint.
+
+    Parameters
+    ----------
+    scan_id : str
+        The name of the scan dataset containing the file.
+    fileset_id : str
+        The name of the fileset containing the file.
+    file_id : str
+        The name of the file.
+
+    Returns
+    -------
+    str
+        The URL path to the file endpoint.
+
+    Examples
+    --------
+    >>> from plantdb.client import api_endpoints
+    >>> api_endpoints.file('real_plant','images','00000_rgb')
+    '/files/real_plant/images/00000_rgb'
+    """
+    scan_id = sanitize_name(scan_id)
+    fileset_id = sanitize_name(fileset_id)
+    file_id = sanitize_name(file_id)
+    return f"/file/{scan_id}/{fileset_id}/{file_id}"
+
+
+@url_prefix
+def metadata_file(scan_id: str, fileset_id: str, file_id: str, **kwargs) -> str:
+    """URL to access the file metadata associated with the given scan and fileset name.
+
+    Parameters
+    ----------
+    scan_id : str
+        The name of the scan to access.
+    fileset_id : str
+        The name of the fileset to access.
+    file_id : str
+        The name of the file to access.
+
+    Other Parameters
+    ----------------
+    prefix : str
+        An optional prefix to prepend to the URL path.
+
+    Returns
+    -------
+    str
+        The URL path to file metadata.
+    """
+    scan_id = sanitize_name(scan_id)
+    fileset_id = sanitize_name(fileset_id)
+    file_id = sanitize_name(file_id)
+    return f"/file/{scan_id}/{fileset_id}/{file_id}/metadata"
+
+
+# ------------------------------------------------------------------------
+# - Asset Endpoints
+# ------------------------------------------------------------------------
 
 @url_prefix
 def image(scan_id: str, fileset_id: str, file_id: str, size: str, as_base64: bool, **kwargs) -> str:
@@ -454,36 +657,6 @@ def archive(scan_id: str, **kwargs) -> str:
 
 
 @url_prefix
-def file(scan_id: str, fileset_id: str, file_id: str, **kwargs) -> str:
-    """Return the URL path to the `scan/fileset/file` endpoint.
-
-    Parameters
-    ----------
-    scan_id : str
-        The name of the scan dataset containing the file.
-    fileset_id : str
-        The name of the fileset containing the file.
-    file_id : str
-        The name of the file.
-
-    Returns
-    -------
-    str
-        The URL path to the file endpoint.
-
-    Examples
-    --------
-    >>> from plantdb.client import api_endpoints
-    >>> api_endpoints.file('real_plant','images','00000_rgb')
-    '/files/real_plant/images/00000_rgb'
-    """
-    scan_id = sanitize_name(scan_id)
-    fileset_id = sanitize_name(fileset_id)
-    file_id = sanitize_name(file_id)
-    return f"/file/{scan_id}/{fileset_id}/{file_id}"
-
-
-@url_prefix
 def file_path(scan_id: str, file_path: str, **kwargs) -> str:
     """Return the URL path to the `scan/file_path` endpoint.
 
@@ -507,93 +680,6 @@ def file_path(scan_id: str, file_path: str, **kwargs) -> str:
     """
     scan_id = sanitize_name(scan_id)
     return f"/files/{scan_id}/{file_path.lstrip('/')}"
-
-
-@url_prefix
-def create_user(**kwargs) -> str:
-    """Create the user registration URL.
-
-    Returns
-    -------
-    str
-        The URL path for user registration.
-
-    Examples
-    --------
-    >>> from plantdb.client import api_endpoints
-    >>> api_endpoints.create_user()
-    '/register'
-    """
-    return "/register"
-
-
-@url_prefix
-def list_scan_filesets(scan: str, **kwargs) -> str:
-    """URL to list the filesets associated with the given scan name.
-
-    Returns
-    -------
-    str
-        The URL path to filesets.
-    """
-    scan_id = sanitize_name(scan)
-    return f"/scan/{scan_id}/filesets"
-
-
-@url_prefix
-def list_fileset_files(scan: str, fileset: str, **kwargs) -> str:
-    """URL to list the file associated with the given scan and filesets names.
-
-    Returns
-    -------
-    str
-        The URL path to filesets.
-    """
-    scan_id = sanitize_name(scan)
-    fileset = sanitize_name(fileset)
-    return f"/scan/{scan_id}/{fileset}/files"
-
-
-@url_prefix
-def metadata_scan(scan: str, **kwargs) -> str:
-    """URL to access the metadata associated with the given scan name.
-
-    Returns
-    -------
-    str
-        The URL path to scan metadata.
-    """
-    scan_id = sanitize_name(scan)
-    return f"/scan/{scan_id}/metadata"
-
-
-@url_prefix
-def metadata_fileset(scan: str, fileset: str, **kwargs) -> str:
-    """URL to access the fileset metadata associated with the given scan and fileset name.
-
-    Returns
-    -------
-    str
-        The URL path to fileset metadata.
-    """
-    scan_id = sanitize_name(scan)
-    fileset = sanitize_name(fileset)
-    return f"/scan/{scan_id}/{fileset}/metadata"
-
-
-@url_prefix
-def metadata_files(scan: str, fileset: str, file: str, **kwargs) -> str:
-    """URL to access the file metadata associated with the given scan and fileset name.
-
-    Returns
-    -------
-    str
-        The URL path to file metadata.
-    """
-    scan_id = sanitize_name(scan)
-    fileset = sanitize_name(fileset)
-    file = sanitize_name(file)
-    return f"/scan/{scan_id}/{fileset}/{file}/metadata"
 
 
 @url_prefix
